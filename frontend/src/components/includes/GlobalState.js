0795746c3369e4ad93018f4a481e545feff780db
Andreaas Pandu P <andspemail@gmail.com>
2024-03-13 10:07:16 +0700

    Update API

--- frontend/src/components/includes/GlobalState.js
+++ frontend/src/components/includes/GlobalState.js
@@ -2,6 +2,6 @@ import { createContext } from "react";
 
 export const GlobalState = {
     title_web: "E-Commerce Paket Data",
-    url_api: "http://localhost:3000/",
+    url_api: "http://localhost:3000",
 }
 export const Context = createContext();
