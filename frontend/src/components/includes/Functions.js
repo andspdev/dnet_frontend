d7f06e814c3b7f51b27256f04a48ad1c493b8ff7
Andreaas Pandu P <andspemail@gmail.com>
2024-03-14 21:33:38 +0700

    Update functions getCookie

--- frontend/src/components/includes/Functions.js
+++ frontend/src/components/includes/Functions.js
@@ -25,10 +25,10 @@ export const getCookie = (cookieName) => {
 
 	for (let i = 0; i < cookieArray.length; i++) {
 		let cookie = cookieArray[i];
-		while (cookie.charAt(0) == " ") {
+		while (cookie.charAt(0) === " ") {
 			cookie = cookie.substring(1);
 		}
-		if (cookie.indexOf(name) == 0) {
+		if (cookie.indexOf(name) === 0) {
 			return cookie.substring(name.length, cookie.length);
 		}
 	}
