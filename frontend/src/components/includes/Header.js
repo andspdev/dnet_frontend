9c94ce83dcbbbc925ff6f109d89eb712957f5beb
Andreaas Pandu P <andspemail@gmail.com>
2024-03-14 21:14:39 +0700

    Update header fixed top

--- frontend/src/components/includes/Header.js
+++ frontend/src/components/includes/Header.js
@@ -6,43 +6,47 @@ export const Header = () =>
     const getUser = getCookie('user_login')
 
     return(
-        <nav className='navbar navbar-expand-lg navbar-custom'>
-            <div className='container-fluid'>
-                <Link to="/" className='navbar-brand'>Logo</Link>
+        <>
+            <nav className='navbar navbar-expand-lg navbar-custom fixed-top'>
+                <div className='container-fluid'>
+                    <Link to="/" className='navbar-brand'>Logo</Link>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link mx-2" to="/">Beranda</Link>
-                        </li>
-
-                        {getUser !== '' ? (
-                            <>
-                                <li className="nav-item">
-                                    <Link className="nav-link mx-2" to='/transaksi'>Transaksi Saya</Link>
-                                </li>
-
-                                <li className="nav-item">
-                                    <Link className="nav-link mx-2" to='/profil'>Profil Saya</Link>
-                                </li>
-
-                                <li className="nav-item">
-                                    <Link className="nav-link mx-2" to='/logout'>Keluar</Link>
-                                </li>
-                            </>
-                            
-                        ) : (
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link mx-2" to='/login'>Login</Link>
+                                <Link className="nav-link mx-2" to="/">Beranda</Link>
                             </li>
-                        )}
-                    </ul>
+
+                            {getUser !== '' ? (
+                                <>
+                                    <li className="nav-item">
+                                        <Link className="nav-link mx-2" to='/transaksi'>Transaksi Saya</Link>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <Link className="nav-link mx-2" to='/profil'>Profil Saya</Link>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <Link className="nav-link mx-2" to='/logout'>Keluar</Link>
+                                    </li>
+                                </>
+                                
+                            ) : (
+                                <li className="nav-item">
+                                    <Link className="nav-link mx-2" to='/login'>Login</Link>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+
+            <div style={{paddingBottom: '5em'}}></div>
+        </>
     )
 }
