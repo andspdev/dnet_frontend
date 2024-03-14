e151126da57b614ba6c0f2e8a163eb0e2407d033
Andreaas Pandu P <andspemail@gmail.com>
2024-03-14 23:08:10 +0700

    update title web

--- frontend/src/components/includes/GlobalState.js
+++ frontend/src/components/includes/GlobalState.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
 export const GlobalState = {
-    title_web: "E-Commerce Paket Data",
+    title_web: "Paketanly : Situs Paket Data Internet All Operator",
     url_api: "http://localhost:3000",
     user_detail: '',
     terakhir_pilih_item: ''
