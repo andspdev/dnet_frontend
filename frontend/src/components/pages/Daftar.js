d362c6a86220c18946bac7e9f1ba4537b10547e6
Andreaas Pandu P <andspemail@gmail.com>
2024-03-14 22:26:32 +0700

    Halaman Daftar (Done)

--- frontend/src/components/pages/Daftar.js
+++ frontend/src/components/pages/Daftar.js
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { Link, useNavigate } from 'react-router-dom'
 import { Context } from "../includes/GlobalState";
-import { getCookie } from '../includes/Functions'
+import { getCookie, getCurrentDateTime } from '../includes/Functions'
 import axios from 'axios'
 
 const Daftar = () =>
@@ -12,9 +12,11 @@ const Daftar = () =>
     const [ stateLocal, setStateLocal ] = useState({
         proses_register: false,
         error_msg: '',
+        input_nama: '',
         input_email: '',
         input_password: '',
-        input_confirm: ''
+        input_confirm: '',
+        email_already: false
     })
 
     if (get_user !== '')
@@ -23,6 +25,66 @@ const Daftar = () =>
     else
     {
         document.title = 'Daftar | '+ stateGlobal.title_web
+        
+        const setErrorMessage = (message) =>
+        {
+            setStateLocal(prevState => ({
+                ...prevState,
+                error_msg: message
+            }))
+        }
+
+        const submitForm = (e) =>
+        {
+            e.preventDefault()
+
+            const input_nama = stateLocal.input_nama.trim()
+            const input_email = stateLocal.input_email.trim()
+            const input_password = stateLocal.input_password
+            const input_confirm = stateLocal.input_confirm
+
+            if (!(/^[a-zA-Z\s]+$/.test(input_nama)) || input_nama === '')
+                setErrorMessage('Nama lengkap tidak valid.')
+
+            else if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input_email)))
+                setErrorMessage('Email tidak valid.')
+
+            else if (!(/^.{6,}$/.test(input_password)))
+                setErrorMessage('Kata sandi minimal 6 karakter.')
+
+            else if (input_confirm !== input_password)
+                setErrorMessage('Konfirmasi kata sandi tidak sama.')
+
+            else
+            {
+                if (stateLocal.email_already)
+                    setErrorMessage('Email telah digunakan oleh pengguna lain.')
+
+                else
+                {
+                    axios({
+                        url: `${stateGlobal.url_api}/pengguna`,
+                        method: 'POST',
+                        data: {
+                            "nama": input_nama,
+                            "email": input_email,
+                            "password": input_password,
+                            "bergabung_pada": new Date().getTime()
+                        }
+                    })
+                    .then(data => {
+                        const response = data.data;
+                        
+                        if (response !== undefined || response !== '')
+                            navigate('/login')
+                    })
+                    .catch(error => {
+                        alert(`Terjadi kesalahan saat daftar akun. (Error: ${error.message})`)
+                        console.log('Error Daftar Akun: ', error);
+                    });
+                }
+            }
+        }
 
         return (
             <div className="box-login">
@@ -31,25 +93,47 @@ const Daftar = () =>
                     <p className="text-muted">Daftar dan buat akun kamu secara gratis disini.</p>
                 </div>
 
-                <form method="post">
+                <form method="post" onSubmit={(e) => submitForm(e)}>
+                    {stateLocal.error_msg !== '' ? (
+                        <div className="alert alert-danger">
+                            {stateLocal.error_msg}
+                        </div>
+                    ) : ''}
+
                     <div className="mb-3">
                         <label htmlFor="nama_lengkap" className="form-label">Nama Lengkap</label>
-                        <input type="text" id="nama_lengkap" placeholder="Nama Lengkap" className="form-control" disabled={stateLocal.proses_register}/>
+                        <input type="text" id="nama_lengkap" placeholder="Nama Lengkap" className="form-control" disabled={stateLocal.proses_register}
+                        onChange={(e) => setStateLocal(prevState => ({
+                            ...prevState,
+                            input_nama: e.target.value
+                        }))}/>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input type="email" id="email" placeholder="Email" className="form-control" disabled={stateLocal.proses_register}/>
+                        <input type="email" id="email" placeholder="Email" className="form-control" disabled={stateLocal.proses_register}
+                        onChange={(e) => setStateLocal(prevState => ({
+                            ...prevState,
+                            input_email: e.target.value
+                        }))}/>
                     </div>
 
                     <div className="mb-4">
                         <label htmlFor="password" className="form-label">Kata Sandi</label>
-                        <input type="password" id="password" placeholder="Kata Sandi" className="form-control" disabled={stateLocal.proses_register} />
+                        <input type="password" id="password" placeholder="Kata Sandi" className="form-control" disabled={stateLocal.proses_register}
+                        onChange={(e) => setStateLocal(prevState => ({
+                            ...prevState,
+                            input_password: e.target.value
+                        }))}/>
                     </div>
 
                     <div className="mb-4">
                         <label htmlFor="konfirmasi_password" className="form-label">Konfirmasi Sandi</label>
-                        <input type="password" id="konfirmasi_password" placeholder="Konfirmasi Sandi" className="form-control" disabled={stateLocal.proses_register} />
+                        <input type="password" id="konfirmasi_password" placeholder="Konfirmasi Sandi" className="form-control" disabled={stateLocal.proses_register}
+                        onChange={(e) => setStateLocal(prevState => ({
+                            ...prevState,
+                            input_confirm: e.target.value
+                        }))} />
                     </div>
 
                     <div className="d-grid mb-4">
