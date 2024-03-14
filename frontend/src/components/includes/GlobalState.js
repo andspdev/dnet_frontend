20d6a9d7ea5bc46b03d495d16e51e7a7b4cb8dcf
Andreaas Pandu P <andspemail@gmail.com>
2024-03-14 23:23:36 +0700

    Update url api

--- frontend/src/components/includes/GlobalState.js
+++ frontend/src/components/includes/GlobalState.js
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 export const GlobalState = {
     title_web: "Paketanly : Situs Paket Data Internet All Operator",
-    url_api: "http://localhost:3000",
+    url_api: "http://localhost:3001",
     user_detail: '',
     terakhir_pilih_item: ''
 }
