780568310945e7bf91d386af71d201295e54dcbc
Andreaas Pandu P <andspemail@gmail.com>
2024-03-14 21:12:46 +0700

    Update Home (Done)

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useContext, useState } from "react";
 import { Header } from "../includes/Header";
 import ImageMobileBrowser from '../../assets/images/mobile-browsers.svg'
 import ImageLoader from '../../assets/images/loader.svg'
@@ -6,40 +6,38 @@ import { nomorHPOperator, formatUang } from '../includes/Functions'
 import { Context } from "../includes/GlobalState";
 import axios from 'axios'
 import { Link } from "react-router-dom";
-import ItemTerakhir from "../includes/ItemTerakhir";
 
-class Home extends Component
+const Home = () =>
 {
-    static contextType = Context;
+    const [stateGlobal] = useContext(Context)
+    const [stateLocal, setStateLocal] = useState({
+        nomor_telepon: '',
+        data_paketan: [],
+        load_data_paketan: true,
+        is_submit_paketan: false
+    })
 
-    constructor(props)
-    {
-        super(props)
+    document.title = stateGlobal.title_web
 
-        this.state = {
-            nomor_telepon: '',
-            data_paketan: [],
-            load_data_paketan: true,
-            is_submit_paketan: false
-        }
-    }
-
-    cekPaketData(e)
+    const cekPaketData = (e) =>
     {
         e.preventDefault();
-        const [getState] = this.context
-        const cek_operator = nomorHPOperator(this.state.nomor_telepon)
 
-        this.setState({
+        const cek_operator = nomorHPOperator(stateLocal.nomor_telepon)
+
+        setStateLocal(prevState => ({
+            ...prevState,
             is_submit_paketan: true,
             load_data_paketan: true
-        })
+        }))
+
 
         axios({
-            url: `${getState.url_api}/paket_data`,
+            url: `${stateGlobal.url_api}/paket_data`,
             method: 'GET'
         })
-        .then(data => {
+        .then(data => 
+        {
             const response = data.data
 
             let get_data_operator = [];
@@ -49,28 +47,31 @@ class Home extends Component
                     get_data_operator.push(value)
             });
 
-            this.setState({
+            setStateLocal(prevState => ({
+                ...prevState,
                 is_submit_paketan: true,
                 load_data_paketan: false,
                 data_paketan: get_data_operator
-            })
+            }))
         })
         .catch(error => {
             alert(`Terjadi kesalahan saat mengambil data. (Error: ${error.message})`)
             console.log('Error Paket Data: ', error)
 
-            this.setState({
+            setStateLocal(prevState => ({
+                ...prevState,
                 is_submit_paketan: false,
                 load_data_paketan: false
-            })
+            }))
         })
     }
 
-    getDataPaketanLayout()
-    {
-        const get_data = this.state.data_paketan
 
-        return(
+    const getDataPaketanLayout = () =>
+    {
+        const get_data = stateLocal.data_paketan
+
+        return (
             <>
                 <div className="row">
                     {
@@ -84,7 +85,7 @@ class Home extends Component
                                         <p>{formatUang(value.harga)}</p>
 
                                         <div className="mt-5">
-                                            <Link to={'/paket-data/'+value.id} state={{nomor_telepon: this.state.nomor_telepon}} className="btn btn-primary btn-sm btn-beli-skrg">Beli Sekarang</Link>
+                                            <Link to={'/paket-data/'+value.id} state={{nomor_telepon: stateLocal.nomor_telepon}} className="btn btn-primary btn-sm btn-beli-skrg">Beli Sekarang</Link>
                                         </div>
                                     </div>
                                 </div>
@@ -96,88 +97,100 @@ class Home extends Component
         )
     }
 
-    render() 
+    const inputNoHp = (e) =>
     {
+        const value = e.target.value
 
-        return(
-            <>
-                <Header/>
+        if (value === '')
+        {
+            setStateLocal(prevState => ({
+                ...prevState,
+                data_paketan: [],
+                is_submit_paketan: false
+            }))            
+        }
 
-                <div className="jumbotron-section">
-                    <div className="description">
-                        <div className="row align-items-center">
-                            <div className="col-md-4 mb-5 text-center text-md-start">
-                                <img src={ImageMobileBrowser} alt="Mobile Browser" className="me-2" />
-                            </div>
-                            <div className="col-md-8 text-center text-md-start">
-                                <div className="mb-4">
-                                    <h2>Paket Data Internet All Operator</h2>
-                                    <p>Isi ulang dengan mudah, nggak pakai ribet dan paling murah. Ayo beli paket data kamu disini!</p>
-                                </div>
+        setStateLocal(prevState => ({
+            ...prevState,
+            nomor_telepon: value
+        }))
+    }
+
+
+    return(
+        <>
+            <Header/>
+
+            <div className="jumbotron-section">
+                <div className="description">
+                    <div className="row align-items-center">
+                        <div className="col-md-4 mb-5 text-center text-md-start">
+                            <img src={ImageMobileBrowser} alt="Mobile Browser" className="me-2" />
+                        </div>
+                        <div className="col-md-8 text-center text-md-start">
+                            <div className="mb-4">
+                                <h2>Paket Data Internet All Operator</h2>
+                                <p>Isi ulang dengan mudah, nggak pakai ribet dan paling murah. Ayo beli paket data kamu disini!</p>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div className="container">
-                    <div className="form-nomor-telepon">
-                        <form method="post" onSubmit={(e) => this.cekPaketData(e)}>
-                            <div className="mb-3">
-                                <label className="form-label" htmlFor="input_nomor_hp">Ayo isi paket datamu di sini</label>
-                                <input type="text" placeholder="Nomor telepon (08xxx)" className="form-control" onChange={(e) => this.setState({
-                                    nomor_telepon: e.target.value
-                                })} />
-                            </div>
+            <div className="container mb-5">
+                <div className="form-nomor-telepon">
+                    <form method="post" onSubmit={(e) => cekPaketData(e)}>
+                        <div className="mb-3">
+                            <label className="form-label" htmlFor="input_nomor_hp">Ayo isi paket datamu di sini</label>
+                            <input type="text" placeholder="Nomor telepon (08xxx)" className="form-control" onChange={(e) => inputNoHp(e)} />
+                        </div>
 
-                            <div className="text-center">
-                                <button className="btn btn-primary">Lihat Paket Data</button>
-                            </div>
-                        </form>
+                        <div className="text-center">
+                            <button className="btn btn-primary">Lihat Paket Data</button>
+                        </div>
+                    </form>
 
 
-                        {!this.state.is_submit_paketan ? '' : 
-                        
-                        this.state.load_data_paketan ? (
-                            <div className="list-paket my-5">
-                                <hr/>
-                                <div className="mt-4">
-                                    Pilih Paket
+                    {!stateLocal.is_submit_paketan ? '' : 
+                    
+                    stateLocal.load_data_paketan ? (
+                        <div className="list-paket my-5">
+                            <hr/>
+                            <div className="mt-4">
+                                Pilih Paket
 
-                                    <div className="loader mt-4">
-                                        <img src={ImageLoader} alt="Loader" />
-                                    </div>
+                                <div className="loader mt-4">
+                                    <img src={ImageLoader} alt="Loader" />
                                 </div>
                             </div>
-                        ) : this.state.data_paketan.length === 0 ? (
-                            <div className="list-paket my-5">
-                                <hr/>
-                                <div className="mt-4">
-                                    Pilih Paket
-                                    
-                                    <div className="text-center mt-4">
-                                        <p>Tidak ada data paketan dari Operator Anda.</p>
-                                    </div>
+                        </div>
+                    ) : stateLocal.data_paketan.length === 0 ? (
+                        <div className="list-paket my-5">
+                            <hr/>
+                            <div className="mt-4">
+                                Pilih Paket
+                                
+                                <div className="text-center mt-4">
+                                    <p>Tidak ada data paketan dari Operator Anda.</p>
                                 </div>
                             </div>
-                        ) : (
-                            <div className="list-paket my-5">
-                                <hr/>
-                                <div className="mt-4">
-                                    Pilih Paket     
+                        </div>
+                    ) : (
+                        <div className="list-paket my-5">
+                            <hr/>
+                            <div className="mt-4">
+                                Pilih Paket     
 
-                                    <div className="mt-4">
-                                        {this.getDataPaketanLayout()}
-                                    </div>
+                                <div className="mt-4">
+                                    {getDataPaketanLayout()}
                                 </div>
                             </div>
-                        )}
-                    </div>
+                        </div>
+                    )}
                 </div>
-
-                <ItemTerakhir />
-            </>
-        )
-    }
+            </div>
+        </>
+    )
 }
 
 export default Home
